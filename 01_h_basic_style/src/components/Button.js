e43a42bb60aa78be43685bcb1183a5a7d14815d3
Komal Dumaniya <komal27dumaniya@gmail.com>
2025-03-19 16:47:36 +0530

    style to react component

---
+++ 01_h_basic_style/src/components/Button.js
@@ -0,0 +1,15 @@
+const Button = () => {
+
+    return (
+        <div>
+            <button type="button" className="btn btn-primary">Primary</button><br />
+            <button type="button" className="btn btn-secondary">Secondary</button><br />
+            <button type="button" className="btn btn-danger">Danger</button>
+
+        </div>
+    );
+}
+
+export { Button };
+
+
