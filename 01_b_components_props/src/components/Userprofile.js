58bf5440b65913b6e0f5fc7e47f2e2e0101a7977
Komal Dumaniya <komal27dumaniya@gmail.com>
2025-03-20 14:57:39 +0530

    changed components

--- 01_b_components_props/src/components/Userprofile.js
+++ 01_b_components_props/src/components/Userprofile.js
@@ -1,9 +1,9 @@
 const Userprofile = ({user}) => {
     return (
       <div >
-          <h2>Name : - {user.name}</h2>
-          <img src={user.image} alt={user.name} />
-          <p>Bio : - {user.bio}</p>
+        <h2>Name : - {user.name}</h2>
+        <img src={user.image} alt={user.name} />
+        <p>Bio : - {user.bio}</p>
       </div>
     );
   };
