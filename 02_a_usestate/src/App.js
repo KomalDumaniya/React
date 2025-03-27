194aa990cd8995651febab6868c3f7c1dbe6d75a
Komal Dumaniya <komal27dumaniya@gmail.com>
2025-03-27 15:44:19 +0530

    Usestate Hook

--- 02_a_usestate/src/App.js
+++ 02_a_usestate/src/App.js
@@ -5,10 +5,10 @@ function App() {
 
   return (
     <div className={darkMode ? "bg-gray-900 text-white" : "bg-white text-black"} style={{ height: "100vh" }}>
-      <div className="p-4 flex justify-center items-center">
+      <div className="p-4 flex justify-right items-right">
         <button
           onClick={() => setDarkMode(!darkMode)}
-          className="px-4 py-2 bg-blue-500 text-white rounded-lg shadow-md"
+          className="px-4 py-2 bg-blue-900 text-white rounded-lg shadow-md"
         >
           {darkMode ? "Light Mode" : "Dark Mode"}
         </button>
