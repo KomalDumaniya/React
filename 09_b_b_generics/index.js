693fb7a5ae98d3e904d01c89fd74b0d16d2b84d4
Komal Dumaniya <komal27dumaniya@gmail.com>
2025-05-14 16:31:23 +0530

    Generics

---
+++ 09_b_b_generics/index.js
@@ -0,0 +1,10 @@
+"use strict";
+function printVar(val) {
+    console.log("data: ", val);
+}
+let arr = [1, 2, 3];
+let arr1 = ["apple", "banana", "cherry"];
+let arr2 = [true, false, true];
+printVar(arr);
+printVar(arr1);
+printVar(arr2);
---
+++ 09_b_b_generics/index.ts
@@ -0,0 +1,11 @@
+function printVar<T>(val: T) { 
+    console.log("data: ", val);
+}
+
+let arr = [1, 2, 3];
+let arr1 = ["apple", "banana", "cherry"];
+let arr2 = [true, false, true];
+
+printVar(arr);
+printVar(arr1);
+printVar(arr2);
